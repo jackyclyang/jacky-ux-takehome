7567a94d0ffe237e8118f92c8d0163dcdb97b9a2
Mark Watabe <mark@tulip.co>
2020-09-01 10:29:16 -0400

    remove unused import

--- src/EDIT_HERE/components/status-bar/index.tsx
+++ src/EDIT_HERE/components/status-bar/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import _ from "lodash";
 
 import ComputationIcon from "./ComputationIcon";
 import BiologicalIcon from "./BiologicalIcon";
